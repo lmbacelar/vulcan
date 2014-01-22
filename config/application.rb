1aea7247f778b3a2542e56a2dbd0924b4e92d400
Luis Bacelar <lmbacelar@gmail.com>
2014-01-22 00:49:29 +0000

    autoloading lib path and i18n setup

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,11 @@ module Vulcan
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # add lib to autoload paths
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    # Set default locales behaviour
+    config.i18n.enforce_available_locales = true
   end
 end
