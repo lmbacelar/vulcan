136516e06e3da750dc0a528824e224a191e83d1e
Luis Bacelar <lmbacelar@gmail.com>
2014-01-23 20:09:51 +0000

    rescue RangeError as it is handled by validations

--- app/models/iec60751/temperature_computation.rb
+++ app/models/iec60751/temperature_computation.rb
@@ -6,7 +6,7 @@ class Iec60751::TemperatureComputation < ActiveRecord::Base
   after_validation :update_temperature
 
   def update_temperature
-    self.temperature = Iec60751.t90 resistance, r0, a, b, c
+    self.temperature = Iec60751.t90 resistance, r0, a, b, c rescue RangeError
   end
 
   def self.table_name_prefix
